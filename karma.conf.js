b98f8e9d6a80364fb2819e0168f326ee7e0bcf58
findriver <riverlaw4@gmail.com>
2014-10-08 11:11:24 -0400

    ph

--- karma.conf.js
+++ karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
 	  'karma-junit-reporter', 
 	  'karma-chrome-launcher',
 	  'karma-coverage' 
-	  'karma-PhantomJS-launcher'
+	  'karma-phantomJS-launcher'
       
 	  ]
   });
